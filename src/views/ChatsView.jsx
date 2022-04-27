1846990168c48c7eb50c1ba497ca16686c19c91a
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-04-27 16:35:41 +0200

    Some changes

--- src/views/ChatsView.jsx
+++ src/views/ChatsView.jsx
@@ -1,14 +1,28 @@
+import { Button } from '@mui/material';
+
 export default function ChatsView() {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
-    <div class="chat-popup" id="myForm">
-  <form action="/action_page.php" class="form-container">
-    <h1>Chat</h1>
+    return (
+        <div class="chat-popup" id="myForm">
+            <form action="/action_page.php" class="form-container">
+                <h1>Chat</h1>
 
-    <label for="msg"><b>Message</b></label>
-    <textarea placeholder="Type message.." name="msg" required></textarea>
+                <label for="msg">
+                    <b>Message</b>
+                </label>
+                <textarea
+                    placeholder="Type message.."
+                    name="msg"
+                    required
+                ></textarea>
 
-    <button type="submit" class="btn">Send</button>
-    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-  </form>
-</div>
+                <button type="submit" class="btn">
+                    Send
+                </button>
+                <button type="button" class="btn cancel" onclick="closeForm()">
+                    Close
+                </button>
+            </form>
+        </div>
+    );
 }
