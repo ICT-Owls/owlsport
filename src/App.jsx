d52dff0349c11a68fb3a351b2ccb3e0ea31f3597
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-04-27 14:35:39 +0200

    Removed the placeholders on the startpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,17 @@ import Button from '@mui/material/Button';
 import './App.css';
 import SidebarPresenter from './presenters/SidebarPresenter';
 import NavbarPresenter from './presenters/NavbarPresenter';
-import ChatsView from './views/ChatsView';
+import ChatsPresenter from './presenters/ChatsPresenter';
 import MainContentPresenter from './presenters/MainContentPresenter';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>it's carpooling time!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Button variant="contained">Look, an MUI button!</Button>
-                    <SidebarPresenter />
-                    <NavbarPresenter />
-                    <ChatsView />
-                    <MainContentPresenter />
-                </a>
-            </header>
+            <Button variant="contained">Look, an MUI button!</Button>
+            <SidebarPresenter />
+            <NavbarPresenter />
+            <ChatsPresenter />
+            <MainContentPresenter />
         </div>
     );
 }
