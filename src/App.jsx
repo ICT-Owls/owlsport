4e4810f1d93710ea492c1fc2edd58c82381a59c9
Hugo Sandell <hugossandell@gmail.com>
2022-04-27 12:13:14 +0200

    format App.jsx with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import logo from "./logo.svg";
-import Button from "@mui/material/Button";
-import "./App.css";
+import logo from './logo.svg';
+import Button from '@mui/material/Button';
+import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>it's carpooling time!</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					<Button variant="contained">Look, an MUI button!</Button>
-					<SidebarPresenter></SidebarPresenter>
-					<NavbarPresenter></NavbarPresenter>
-					<ChatsView></ChatsView>
-					<MainContentPresenter />
-				</a>
-			</header>
-		</div>
-	);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>it's carpooling time!</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Button variant="contained">Look, an MUI button!</Button>
+                    <SidebarPresenter></SidebarPresenter>
+                    <NavbarPresenter></NavbarPresenter>
+                    <ChatsView></ChatsView>
+                    <MainContentPresenter />
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
