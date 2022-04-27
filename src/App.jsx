ec8e13189b26188b670f23307fc9cf0a54ad3c23
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-04-27 13:36:52 +0200

    Tailwind Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import logo from './logo.svg';
 import Button from '@mui/material/Button';
 import './App.css';
-
+import SidebarPresenter from './presenters/SidebarPresenter';
+import NavbarPresenter from './presenters/NavbarPresenter';
+import ChatsView from './views/ChatsView';
+import MainContentPresenter from './presenters/MainContentPresenter';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>it's carpooling time!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Button variant="contained">Look, an MUI button!</Button>
-                    <SidebarPresenter></SidebarPresenter>
-                    <NavbarPresenter></NavbarPresenter>
-                    <ChatsView></ChatsView>
-                    <MainContentPresenter />
-                </a>
-            </header>
+            <Button variant="contained">Look, an MUI button!</Button>
+            <SidebarPresenter></SidebarPresenter>
+            <NavbarPresenter></NavbarPresenter>
+            <ChatsView></ChatsView>
+            <MainContentPresenter />
         </div>
     );
 }
--- src/views/NavbarView.jsx
+++ src/views/NavbarView.jsx
@@ -1,4 +1,4 @@
 export default function NavbarView() {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
-    return <h1>test</h1>;
+    return <h1 className="text-5xl hover:bg-cyan-300">test</h1>;
 }
