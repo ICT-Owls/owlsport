ece107b5bc1f882418ae1de4b0d2b62d83fc00c1
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-04-27 09:39:22 +0200

    Some initial presenter and view files

---
+++ src/presenters/SidebarPresenter.js
@@ -0,0 +1,6 @@
+import SidebarView from "../views/SidebarView";
+export default function SidebarPresenter() {
+	// Hooks, logic, etc goes here. These presenters manipulate data, transform it into usable functions and values, then passes those to a view.
+	// No visual code here.
+	return SidebarView();
+}
---
+++ src/views/MainContentView.jsx
@@ -0,0 +1,4 @@
+export default function MainContentView() {
+	//These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
+	return <h1>test</h1>;
+}
