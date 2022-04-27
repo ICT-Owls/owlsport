cca437586ae299d6171a59b730a64cba4cb8d991
VolcanoCookies <franek.kg@gmail.com>
2022-04-27 16:18:43 +0200

    Added database import and validation chain

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import { body, validationResult } from 'express-validator';
 const admin = require('firebase-admin');
 const serviceAccount = require('../***REMOVED***-firebase-adminsdk-wf7mw-0e8fb32b51.json');
 
@@ -6,6 +7,7 @@ admin.initializeApp({
     databaseURL:
         '***REMOVED***',
 });
+const database = admin.database();
 
 const express = require('express');
 const path = require('path');
@@ -31,8 +33,37 @@ app.use(decodeIDToken);
 
 app.use(express.static('build'));
 
+app.get('/user/:id',
+    params('id').isInt().withMessage('Id is not an int')
+    async (req, res) => {
+        const id = req.params.id;
+        if (!id) return res.status(400).send('No id');
+
+        const user = database.ref(`/user/${id}`);
+        if (!user) return res.status(404).send('User not found');
+    });
+
+app.post('/user/:id',
+    body('email').isEmail().withMessage('Invalid email'),
+    body('friends').isArray().withMessage('Friends is not an array'),
+    body('friends.*').isInt().withMessage('Friend id is not an int'),
+    async (req, res) => {
+        const { email, friends } = req.body;
+        if (!email && !friends) return res.status(400).send('Empty body');
+
+    });
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('build', 'index.html'));
+});
+
+app.use(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty())
+        next();
+    else {
+        res.status(400).send(errors);
+    }
 });
 
 app.listen(port, () => {
