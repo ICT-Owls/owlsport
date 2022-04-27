b608af11236f3fadcbca136a942c3605d7645930
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-04-27 17:12:28 +0200

    Starting point

--- src/views/NavbarView.jsx
+++ src/views/NavbarView.jsx
@@ -1,4 +1,34 @@
+import { Box, IconButton, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 export default function NavbarView() {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
-    return <h1 className="text-5xl hover:bg-cyan-300">test</h1>;
+    return (
+        <div className="w-full h-fit fixed top-0 left-0 flex justify-around bg-white bg-opacity-90">
+            <IconButton
+                color="primary"
+                aria-label="SolidSport"
+                component="span"
+            >
+                <img src="Solid_Logotype.png" className="h-6" alt="" />
+            </IconButton>
+            <Box
+                className="m-4 flex justify-start"
+                sx={{
+                    width: 1000,
+                    maxWidth: '100%',
+                }}
+            >
+                <TextField fullWidth label="fullWidth" id="fullWidth" />
+                <IconButton
+                    color="primary"
+                    aria-label="SolidSport"
+                    component="span"
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Box>
+            <div>{/* I act as a third item for formattings sake */}</div>
+        </div>
+    );
 }
