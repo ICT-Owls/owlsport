5b5fdb699fec52ccdbecbbe0727c74ef7f8a1857
VolcanoCookies <franek.kg@gmail.com>
2022-04-28 14:05:20 +0200

    feat: add event fetching, updating and creation api

---
+++ server/routes/eventRoute.js
@@ -0,0 +1,82 @@
+const { body, param } = require('express-validator');
+const { validate } = require('../utils.js');
+const { events } = require('../database.js');
+
+const express = require('express');
+const router = express.Router();
+
+router.post(
+    '/create',
+    body('title').isString(),
+    body('description').isString(),
+    body('startDateTime').isNumeric(),
+    body('endDateTime').isNumeric(),
+    validate,
+    async (req, res) => {
+        const { title, description, startDateTime, endDateTime } = req.body;
+        const authUser = req.user;
+
+        const eventRef = events.push();
+        await eventRef.set({
+            creatorId: authUser.uid,
+            id: eventRef.key,
+            title: title,
+            description: description,
+            startDateTime: startDateTime,
+            endDateTime: endDateTime,
+            creationDate: Date.now(),
+            members: [],
+        });
+
+        const event = await eventRef.get();
+
+        res.send(event.val());
+    }
+);
+
+router.get('/:id', param('id').isString(), validate, async (req, res) => {
+    const id = req.params.id;
+
+    const eventRef = events.child(id);
+    const event = await (await eventRef.get()).val();
+    if (!event) return res.status(404).send('Event not found');
+
+    res.send(event);
+});
+
+router.post(
+    '/:id',
+    body('title').optional().isString(),
+    body('description').optional().isString(),
+    body('members').optional().isArray(),
+    body('members.*').isString(),
+    body('startDateTime').optional({ min: 0 }).isNumeric(),
+    body('endDateTime').optional({ min: 0 }).isNumeric(),
+    validate,
+    async (req, res) => {
+        const id = req.params.id;
+        const { title, description, members, startDateTime, endDateTime } =
+            req.body;
+
+        const eventRef = events.child(id);
+        const eventSnapshot = await eventRef.get();
+
+        if (!eventSnapshot.exists())
+            return res.status(404).send('Event not found');
+
+        var update = {};
+        if (title) update.title = title;
+        if (description) update.description = description;
+        if (members) update.members = members;
+        if (startDateTime) update.startDateTime = startDateTime;
+        if (endDateTime) update.endDateTime = endDateTime;
+
+        await eventRef.update(update);
+
+        const updatedEvent = await eventRef.get();
+
+        res.send(updatedEvent.val());
+    }
+);
+
+module.exports = router;
