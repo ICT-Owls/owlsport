af388fc582d829b676fec3d775cc45f0b84d89f3
Magma980 <syunkova@gmail.com>
2022-04-28 11:26:18 +0200

    Design for sidebars: logged in/out(true/false)

--- src/views/SidebarView.jsx
+++ src/views/SidebarView.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { IconButton, ListItemText, MenuItem, MenuList } from '@mui/material';
+import { IconButton, ListItemText, MenuItem, MenuList, Divider } from '@mui/material';
 
-export default function SidebarView() {
-	//These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
+export default function SidebarView(props) {
+	const isLoggedIn = props.isLoggedIn;
+
+	//These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if necessary)
 	return (
 		<div className= 'sidebar'>
 			<IconButton color="secondary" aria-label="Search" component="span"
@@ -32,58 +34,47 @@ export default function SidebarView() {
 							fontSize: 22,
 							color: 'primary.secondary',
 						}}>Whatever Else</ListItemText>
+						<Divider black />
 					</MenuItem>
 				</MenuList>
 
-				<MenuList>
-					<MenuItem onClick={() => window.location.pathname = '/login'}>
-						<ListItemText primaryTypographyProps={{
-							fontSize: 22,
-							color: 'primary.secondary',
-						}}>Login</ListItemText>
-					</MenuItem>
-					<MenuItem onClick={() => window.location.pathname = '/signup'}>
-						<ListItemText primaryTypographyProps={{
-							fontSize: 22,
-							color: 'primary.secondary',
-						}}>Sign Up</ListItemText>
-					</MenuItem>
-				</MenuList>
-				{/*<ul className='sidebar_ul'>*/}
-				{/*	<li key='0'*/}
-				{/*		className = 'row'*/}
-				{/*		onClick={() => window.location.pathname = '/events'}>*/}
-				{/*		<div>*/}
-				{/*			Events*/}
-				{/*		</div>*/}
-				{/*	</li>*/}
-				{/*	<li key='1' onClick={() => window.location.pathname = '/about'}>*/}
-				{/*		<div>*/}
-				{/*			About Us*/}
-				{/*		</div>*/}
-				{/*	</li>*/}
-				{/*	<li key='2' onClick={() => window.location.pathname = '/whatever'}>*/}
-				{/*		<div>*/}
-				{/*			Whatever Else*/}
-				{/*		</div>*/}
-				{/*	</li>*/}
-				{/*</ul>*/}
+				<Divider/>
+				{
+					isLoggedIn ?
+						(
+							<MenuList>
+								<MenuItem onClick={() => window.location.pathname = '/logout'}>
+									<ListItemText primaryTypographyProps={{
+										fontSize: 22,
+										color: 'primary.secondary',
+									}}>Logout</ListItemText>
+								</MenuItem>
+								<MenuItem onClick={() => window.location.pathname = '/account'}>
+									<ListItemText primaryTypographyProps={{
+										fontSize: 22,
+										color: 'primary.secondary',
+									}}>Account</ListItemText>
+								</MenuItem>
+							</MenuList>
+						) :
+						(
+							<MenuList>
+								<MenuItem onClick={() => window.location.pathname = '/login'}>
+									<ListItemText primaryTypographyProps={{
+										fontSize: 22,
+										color: 'primary.secondary',
+									}}>Login</ListItemText>
+								</MenuItem>
+								<MenuItem onClick={() => window.location.pathname = '/signup'}>
+									<ListItemText primaryTypographyProps={{
+										fontSize: 22,
+										color: 'primary.secondary',
+									}}>Sign Up</ListItemText>
+								</MenuItem>
+							</MenuList>
+						)
+				}
 			</div>
-
-			{/*<div className='sidebar_second'>*/}
-			{/*	<ul  className='sidebar_ul'>*/}
-			{/*		<li key='3' onClick={() => window.location.pathname = '/login'}>*/}
-			{/*			<div>*/}
-			{/*				Login*/}
-			{/*			</div>*/}
-			{/*		</li>*/}
-			{/*		<li key='4' onClick={() => window.location.pathname = '/signup'}>*/}
-			{/*			<div>*/}
-			{/*				Sign Up*/}
-			{/*			</div>*/}
-			{/*		</li>*/}
-			{/*	</ul>*/}
-			{/*</div>*/}
 		</div>
 	);
 }
