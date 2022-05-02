12f00110691d5bf0fddb5d2d08747c98c7b5de8a
Hugo Sandell <hugossandell@gmail.com>
2022-05-02 17:36:58 +0200

    style: fix formatting

--- src/views/NavbarView.jsx
+++ src/views/NavbarView.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function NavbarView() {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
     return <h1 className="text-5xl hover:bg-cyan-300">test</h1>;
