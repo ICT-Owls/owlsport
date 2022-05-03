7cc31883ef6b0fb4e7b6a3ce3653330fee96c8cf
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-05-03 17:39:08 +0200

    Feat: Signin Preliminary functionality

--- src/presenters/SidebarPresenter.js
+++ src/presenters/SidebarPresenter.js
@@ -1,7 +1,22 @@
 import SidebarView from '../views/SidebarView';
+import { useState } from 'react';
 
 export default function SidebarPresenter(props) {
-    // Hooks, logic, etc goes here. These presenters manipulate data, transform it into usable functions and values, then passes those to a view.
-    // No visual code here.
-    return SidebarView(props);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [openLogin, setOpenLogin] = useState(false);
+
+    function handleLoginOpen() {
+        setOpenLogin(true);
+    }
+
+    function handleLoginClose() {
+        setOpenLogin(false);
+    }
+    return SidebarView({
+        openLogin,
+        handleLoginOpen,
+        handleLoginClose,
+        isLoggedIn,
+        setIsLoggedIn,
+    });
 }
--- src/views/SidebarView.jsx
+++ src/views/SidebarView.jsx
@@ -1,78 +1,84 @@
-import { React, useState } from 'react';
-import {
-    IconButton,
-    ListItemText,
-    MenuItem,
-    MenuList,
-    Divider,
-    Button,
-} from '@mui/material';
+import { React } from 'react';
+import { IconButton, Divider, Button } from '@mui/material';
 import PropTypes from 'prop-types';
-import { startLogin } from '../helpers/Firebase';
+import SignInView from './SignInView';
+import { Link } from 'react-router-dom';
 
-export default function SidebarView(props) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+export default function SidebarView({
+    openLogin,
+    handleLoginOpen,
+    handleLoginClose,
+    isLoggedIn,
+    setIsLoggedIn,
+}) {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if necessary)
     return (
-        <div className="sidebar">
-            <IconButton
-                color="secondary"
-                aria-label="Search"
-                component="span"
-                onClick={() => (window.location.pathname = '/')}
-            >
-                <img src="Solid_Logotype.png" className="h-10" alt="" />
-            </IconButton>
-            {/*<div className='logo'>*/}
-            {/*<img src='Solid_Logotype.png' alt='logo'/>*/}
-            {/*</div>*/}
+        <>
+            <div className="sidebar">
+                <IconButton
+                    href="/test"
+                    color="secondary"
+                    aria-label="Search"
+                    component="span"
+                >
+                    <Link to="/">
+                        <img src="Solid_Logotype.png" className="h-10" alt="" />
+                    </Link>
+                </IconButton>
 
-            {/* eslint-disable-next-line tailwindcss/no-custom-classname*/}
-            <div className="sidebar_first">
-                <div className="flex flex-col justify-start child:text-xl child:p-4">
-                    <Button color="primary" href="/events">
-                        Events
-                    </Button>
-                    <Button color="primary" href="/about">
-                        About
-                    </Button>
-                    <Button color="primary" href="/whatever" className="mb-0">
-                        Whatever
-                    </Button>
-                    <Divider style={{ padding: '0px' }} />
-                    {isLoggedIn ? (
-                        <>
-                            <Button color="primary" href="/account">
-                                Account
-                            </Button>
-                            <Button
-                                color="primary"
-                                href="/whatever"
-                                onClick={() => setIsLoggedIn(!isLoggedIn)}
-                            >
-                                Sign Out
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <Button
-                                color="primary"
-                                onClick={() => startLogin()}
-                            >
-                                Sign In
-                            </Button>
-                            <Button
-                                color="primary"
-                                onClick={() => setIsLoggedIn(!isLoggedIn)}
-                            >
-                                Create Account
-                            </Button>
-                        </>
-                    )}
+                <div className="sidebar_first">
+                    <div className="flex flex-col justify-start child:text-xl child:p-4">
+                        <Button color="primary" href="/events">
+                            Events
+                        </Button>
+                        <Button color="primary" href="/about">
+                            About
+                        </Button>
+                        <Button
+                            color="primary"
+                            href="/whatever"
+                            className="mb-0"
+                        >
+                            Whatever
+                        </Button>
+                        <Divider style={{ padding: '0px' }} />
+                        {isLoggedIn ? (
+                            <>
+                                <Button color="primary" href="/account">
+                                    Account
+                                </Button>
+                                <Button
+                                    color="primary"
+                                    href="/whatever"
+                                    onClick={() => setIsLoggedIn(!isLoggedIn)}
+                                >
+                                    Sign Out
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Button
+                                    color="primary"
+                                    onClick={() => handleLoginOpen(true)}
+                                >
+                                    Sign In
+                                </Button>
+                                <Button
+                                    color="primary"
+                                    onClick={() => setIsLoggedIn(!isLoggedIn)}
+                                >
+                                    Create Account
+                                </Button>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+            <SignInView
+                handleClose={() => handleLoginClose}
+                showMe={openLogin}
+            />
+        </>
     );
 }
 
