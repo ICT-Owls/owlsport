65ef80401952f80239f5ffbaab5bd01e8a84cfbc
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-05-03 11:36:30 +0200

    chore: Routing in MUI, also theme change

--- src/Themes.js
+++ src/Themes.js
@@ -1,29 +1,53 @@
+import * as React from 'react';
 import { createTheme } from '@mui/material/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const LinkBehavior = React.forwardRef((props, ref) => {
+    const { href, ...other } = props;
+    // Map href (MUI) -> to (react-router)
+    return (
+        <RouterLink data-testid="custom-link" ref={ref} to={href} {...other} />
+    );
+});
+
+LinkBehavior.displayName = 'Theme.js, i hope this wont break anything';
+
+LinkBehavior.propTypes = {
+    href: PropTypes.oneOfType([
+        PropTypes.shape({
+            hash: PropTypes.string,
+            pathname: PropTypes.string,
+            search: PropTypes.string,
+        }),
+        PropTypes.string,
+    ]).isRequired,
+};
+
 export const LightTheme = createTheme({
     palette: {
         type: 'light',
         primary: {
-            main: '#f5f5f5',
+            main: '#ec1e24',
         },
         secondary: {
-            main: '#ec1e24',
+            main: '#666',
         },
         background: {
             default: '#e0e0e0',
         },
     },
-});
-export const DarkTheme = createTheme({
-    palette: {
-        type: 'light',
-        primary: {
-            main: '#f5f5f5',
+    components: {
+        MuiLink: {
+            defaultProps: {
+                component: LinkBehavior,
+            },
         },
-        secondary: {
-            main: '#ec1e24',
-        },
-        background: {
-            default: '#e0e0e0',
+        MuiButtonBase: {
+            defaultProps: {
+                LinkComponent: LinkBehavior,
+            },
         },
     },
 });
+export const DarkTheme = LightTheme;
