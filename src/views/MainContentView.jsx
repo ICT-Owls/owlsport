b751d2490138b685b5b2b83adf0ac3f11ee59882
VolcanoCookies <franek.kg@gmail.com>
2022-05-04 14:54:20 +0200

    chore: add route for eventlist

--- src/views/MainContentView.jsx
+++ src/views/MainContentView.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import EventListPresenter from '../presenters/EventListPresenter';
 export default function MainContentView() {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
-    return <h1>test</h1>;
+    return (
+        <Routes>
+            <Route path="/events" element={<EventListPresenter />} />
+        </Routes>
+    );
 }
