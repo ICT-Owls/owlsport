f2104fcb51ba78a1b99111eec550eac73727b939
Magma980 <syunkova@gmail.com>
2022-05-04 16:39:18 +0200

    Event Creation new design

--- src/views/EventCreatingView.jsx
+++ src/views/EventCreatingView.jsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 //import { FormControlUnstyled } from '@mui/base';
 import { Button, Grid } from '@mui/material';
-import { Grid, } from '@material-ui/core';
+//import { Grid } from '@material-ui/core';
 //import SearchIcon from '@mui/icons-material/Search';
 import { Wrapper } from "@googlemaps/react-wrapper";
 import { useEffect, useRef } from 'react';
@@ -29,86 +29,30 @@ function MyMapComponent({ center, zoom}) {
     return <div ref={ref} id="map" />;
 }
 
-export default function EventCreatingView() {
-    const [value, setValue] = React.useState(new Date());
-
-    return(
-        // <Form onSubmit={handleSubmit}>
-        <Form>
-            <Grid container>
-                <Grid item xs={6}>
-                    <Controls.Input
-                        name="fullName"
-                        label="Full Name"
-                        value={values.fullName}
-                        onChange={handleInputChange}
-                        error={errors.fullName}
-                    />
-                    <Controls.Input
-                        label="Email"
-                        name="email"
-                        value={values.email}
-                        onChange={handleInputChange}
-                        error={errors.email}
-                    />
-                    <Controls.Input
-                        label="Mobile"
-                        name="mobile"
-                        value={values.mobile}
-                        onChange={handleInputChange}
-                        error={errors.mobile}
-                    />
-                    <Controls.Input
-                        label="City"
-                        name="city"
-                        value={values.city}
-                        onChange={handleInputChange}
-                    />
-
-                </Grid>
-                <Grid item xs={6}>
-                    <Controls.RadioGroup
-                        name="gender"
-                        label="Gender"
-                        value={values.gender}
-                        onChange={handleInputChange}
-                        items={genderItems}
-                    />
-                    <Controls.Select
-                        name="departmentId"
-                        label="Department"
-                        value={values.departmentId}
-                        onChange={handleInputChange}
-                        options={employeeService.getDepartmentCollection()}
-                        error={errors.departmentId}
-                    />
-                    <Controls.DatePicker
-                        name="hireDate"
-                        label="Hire Date"
-                        value={values.hireDate}
-                        onChange={handleInputChange}
-                    />
-                    <Controls.Checkbox
-                        name="isPermanent"
-                        label="Permanent Employee"
-                        value={values.isPermanent}
-                        onChange={handleInputChange}
-                    />
-
-                    <div>
-                        <Controls.Button
-                            type="submit"
-                            text="Submit" />
-                        <Controls.Button
-                            text="Reset"
-                            color="default"
-                            onClick={resetForm} />
-                    </div>
-                </Grid>
-            </Grid>
-        </Form>
-    );
-}
+// const InputBox = params => {
+//     const {placeholder, name, id} = params;
+//     return (
+//         <input
+//             type="text"
+//             placeholder={placeholder}
+//             className="
+//                 w-full
+//                 rounded
+//                 p-3
+//                 text-gray-800
+//                 dark:text-gray-50
+//                 dark:bg-slate-700
+//                 border-gray-500
+//                 dark:border-slate-600
+//                 outline-none
+//                 focus-visible:shadow-none
+//                 focus:border-primary
+//                 "
+//             name="event"
+//             id="event"
+//         />
+//     );
+// };
 
 export default function EventCreatingView2() {
     const [value, setValue] = React.useState(new Date());
@@ -117,70 +61,133 @@ export default function EventCreatingView2() {
     return (
         <div className='container absolut w-screen h-screen bg-black bg-opacity-10 flex justify-center'>
             <div className='flex w-full  justify-center bg-white mt-24 '>
-                {/*<UserInfoView/>*/}
-
                 <div className='m-8'>
-                    <div className='justify-start flex'>
-                        {/*We need to have a global state with user info to display here*/}
+                    <div className='justify-start ml-14 flex relative'>
+
+                        {/*Avatar/UserID*/}
                         <div className=''>
                             <img src='avatar.png' className="h-12" alt='avatar' />
-
                         </div>
 
                         <div  className='ml-8'>
                             <h5 className=''>James Bond</h5>
-
                         </div>
 
-                    </div>{/*md:w-1/2 content-center*/}
-                    <div className='flex'>
-                        <div  className='md:w-2/5 content-center min-w-200'>
-                            <form className=''>
-                                <div className=''>
-                                    <div className=''>
-                                        <TextField className='w-full '
-                                                   label="Event name"
-                                                   id="EventName"
-                                        />
-                                    </div>
-                                    <div  className='w-full mt-4'>
-                                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                            <DateTimePicker
-                                                renderInput={(props) => <TextField {...props} />}
-                                                label="Select Date and Time"
-                                                value={value}
-                                                onChange={(newValue) => {
-                                                    setValue(newValue);
-                                                }}
-                                            />
-                                        </LocalizationProvider>
+                    </div>
+                    <div className='flex w-full px-12 data-aos-duration="2000'>
+                        <div className='bg-gray-100 relative rounded-lg p-8 sm:p-12'>
+                            <div className="mb-6">
+                                <textfield
+                                    type="text"
+                                    placeholder="Event"
+                                    className="
+                                    w-full
+                                    rounded
+                                    p-3
+                                    focus:border-primary
+                                    "
+                                    name="event"
+                                    id="event"
+                                />
+                            </div>
 
+                            <div className="mb-6">
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <DateTimePicker
+                                        renderInput={(props) => <TextField {...props} />}
+                                        label="Select Date and Time"
+                                        className="
+                                    w-full
+                                    rounded
+                                    p-3
+                                    text-gray-800
+                                    dark:text-gray-50
+                                    dark:bg-slate-700
+                                    border-gray-500
+                                    dark:border-slate-600
+                                    outline-none
+                                    focus-visible:shadow-none
+                                    focus:border-primary
+                                    "
+                                        value={value}
+                                        onChange={(newValue) => {
+                                            setValue(newValue);
+                                        }}
+                                    />
+                                </LocalizationProvider>
+                            </div>
+
+                            <div className="mb-6">
+                                <select className="form-select appearance-none
+                                  block
+                                  w-full
+                                  px-3
+                                  py-1.5
+                                  text-base
+                                  font-normal
+                                  text-gray-700
+                                  bg-white bg-clip-padding bg-no-repeat
+                                  border border-solid border-gray-300
+                                  rounded
+                                  transition
+                                  ease-in-out
+                                  m-0
+                                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                                    <option selected>Duration (hours)</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+
+                            <div className="mb-6">
+                                <input
+                                    type="text"
+                                    placeholder="Location"
+                                    className="
+                                    w-full
+                                    rounded
+                                    p-3
+                                    focus:border-primary
+                                                                       "
+                                    name="location"
+                                    id="location"
+                                />
+                            </div>
+
+                            <div className='m-6'>
+                                <div className='flex space-x-2 justify-center'>
+                                    <div>
+                                        <img src='avatar.png' className="h-12" alt='avatar' />
                                     </div>
                                     <div>
-
+                                        <img src='avatar.png' className="h-12" alt='avatar' />
                                     </div>
-
-                                    <div className='m-6'>
-                                        <div className='flex  space-x-2'>
-                                            <div>
-                                                <img src='avatar.png' className="h-12" alt='avatar' />
-                                            </div>
-                                            <div>
-                                                <img src='avatar.png' className="h-12" alt='avatar' />
-                                            </div>
-                                            <div>
-                                                <img src='avatar.png' className="h-12" alt='avatar' />
-                                            </div>
-                                        </div>
+                                    <div>
+                                        <img src='avatar.png' className="h-12" alt='avatar' />
                                     </div>
+                                </div>
+                            </div>
 
-
+                            <form className=''>
+                                <div className=''>
 
                                     <div className='mt-4'>
-                                        <Button variant="contained"
-                                                color="secondary"
-                                                size="medium"
-                                        > Create </Button>
+
+                                        <Button
+                                            variant="contained"
+                                            className="
+                                    w-full
+                                    hover: bg-primary
+                                    rounded
+                                    border border-primary
+                                    p-3
+                                    transition
+                                    duration-500
+                                    "
+                                        >
+                                           Create
+                                        </Button>
                                     </div>
                                 </div>
                             </form>
@@ -188,13 +195,47 @@ export default function EventCreatingView2() {
                         </div>
 
                         <div  className='md:w-3/5 content-center ml-8'>
-                            <div className=''>
-                                <TextField className='w-full '
-                                           label="Description"
-                                           id="Description"
-                                />
+                            <div className="bg-gray-100 relative rounded-lg p-8 sm:p-12">
+                                <textarea
+                                    rows="12"
+                                    placeholder="About"
+                                    className="
+                                    w-full
+                                    rounded
+                                    p-3
+                                    font-family: 'Arial'
+
+                                    "
+                                    name="about"
+                                    id="about"
+                                ></textarea>
+
+                                <div className='m-6'>
+                                    <iframe
+                                        sx={{ border: "0" }}
+                                        w-full
+                                        h-full
+                                        // width="400"
+                                        // height="400"
+                                        // style="border:0"
+                                        loading="lazy"
+                                        frameBorder="0"
+                                        allowFullScreen
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCGxW2vdNkBmPIc4GEer8Y85xAXPpfMjwY&q=Space+Needle,Seattle+WA">
+                                    </iframe>
+                                </div>
+
+
+
                             </div>
-                            <div className="h-full w-300">
+                            {/*<div className=''>*/}
+                            {/*    <TextField className='w-full '*/}
+                            {/*               label="Description"*/}
+                            {/*               id="Description"*/}
+                            {/*    />*/}
+                            {/*</div>*/}
+                            {/*<div className="h-full w-300">*/}
                                 {/*<Wrapper apiKey={"AIzaSyDXvjtauHrSqoYDwlFH_ST899pZVcWY7jo"}>*/}
                                 {/*    <MyMapComponent />*/}
                                 {/*</Wrapper>*/}
@@ -207,24 +248,23 @@ export default function EventCreatingView2() {
                                 {/*    defer*/}
                                 {/*/>*/}
 
-                                <iframe
-                                    sx={{ border: "0" }}
-                                    width="400"
-                                    height="300"
-                                    // style="border:0"
-                                    loading="lazy"
-                                    frameBorder="0"
-                                    allowFullScreen
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCGxW2vdNkBmPIc4GEer8Y85xAXPpfMjwY&q=Space+Needle,Seattle+WA">
-                                </iframe>
-                            </div>
+                                {/*<iframe*/}
+                                {/*    sx={{ border: "0" }}*/}
+                                {/*    width="400"*/}
+                                {/*    height="300"*/}
+                                {/*    // style="border:0"*/}
+                                {/*    loading="lazy"*/}
+                                {/*    frameBorder="0"*/}
+                                {/*    allowFullScreen*/}
+                                {/*    referrerPolicy="no-referrer-when-downgrade"*/}
+                                {/*    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCGxW2vdNkBmPIc4GEer8Y85xAXPpfMjwY&q=Space+Needle,Seattle+WA">*/}
+                                {/*</iframe>*/}
+                            {/*</div>*/}
 
                         </div>
                     </div>
 
                 </div>
-
             </div>
         </div>
         // <div className="event">
