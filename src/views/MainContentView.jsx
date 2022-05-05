a0ea80e94261d52375775579032a546d51b424d3
VolcanoCookies <franek.kg@gmail.com>
2022-05-05 16:55:35 +0200

    fix: remove div that break stuff

--- src/views/MainContentView.jsx
+++ src/views/MainContentView.jsx
@@ -5,17 +5,12 @@ import EventListPresenter from '../presenters/EventListPresenter';
 export default function MainContentView(props) {
     //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
     return (
-        <div>
-            <Routes>
-                <Route
-                    path="/events"
-                    element={<EventListPresenter user={props.user} />}
-                />
-                <Route
-                    path="/"
-                    element={<ChatsPresenter />}
-                />
-            </Routes>
-        </div>
+        <Routes>
+            <Route
+                path="/events"
+                element={<EventListPresenter user={props.user} />}
+            />
+            <Route path="/" element={<ChatsPresenter />} />
+        </Routes>
     );
 }
