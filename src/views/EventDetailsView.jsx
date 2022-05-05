8e27cba4a71b9fde1f211a8196114ee0ec8bd293
Mohammad <mdidehbanmehr@gmail.com>
2022-05-05 16:12:51 +0200

    modal issue resolved

--- src/views/EventDetailsView.jsx
+++ src/views/EventDetailsView.jsx
@@ -40,86 +40,88 @@ export default function EventDetailsView () {
     }));
 
     return (
-        // <div className='flex flex-col justify-around bg-gray-100'>
-        //     <div className='flex flex-row justify-evenly'>
-        //         <div className='flex flex-col justify-start my-3'>
-        //             <div className='text-2xl'>Cool match</div>
-        //             <div className='text-xs'>Organized by Eric Erricson</div>
-        //         </div>
-        //         <div className='flex flex-col justify-start my-3'>
-        //             <Button
-        //                 className='mb-5 w-52 bg-gray-300'
-        //                 sx={{ color: 'black' }}
-        //             >
-        //                 Request ride
-        //             </Button>
-        //             <div className='my-1 w-fit h-fit text-xs text-gray-400'>
-        //                 Thur, April 26, 2066 at 22:30 - Thur, April 26, 2066 at
-        //                 23:30
-        //             </div>
-        //             <div className='my-1 w-fit h-fit text-xs text-gray-400'>
-        //                 kungsträdgården
-        //             </div>
-        //         </div>
-        //         <div className='my-5 w-14 h-14 bg-primary-100'>
-        //             <div className=' text-xl'>APR 23</div>
-        //         </div>
-        //     </div>
-
-        //     <div className='flex flex-row justify-around py-3 my-3 bg-gray-300'>
-        //         <div>Description</div>
-        //         <div>Carpooling</div>
-        //         <div>Events</div>
-        //     </div>
-        //     <div className='flex flex-row justify-around py-3 my-3 bg-gray-300'>
-        //         <CarPooler>CarPooler 1</CarPooler>
-        //         <CarPooler>CarPooler 2</CarPooler>
-        //         <CarPooler>CarPooler 3</CarPooler>
-        //     </div>
-        //     <Divider variant='middle' />
-        //     <div className='my-1 w-fit h-fit text-xs text-gray-700'>
-        //         Lorem Ipsum is simply dummy text of the printing and typesetting
-        //         industry. Lorem Ipsum has been the industries standard dummy
-        //         text ever since the 1500s, when an unknown printer took a galley
-        //         of type and scrambled it to make a type specimen book. It has
-        //         survived not only five centuries, but also the leap into
-        //         electronic typesetting, remaining essentially unchanged. It was
-        //         popularised in the 1960s with the release of Letraset sheets
-        //         containing Lorem Ipsum passages, and more recently with desktop
-        //         publishing software like Aldus PageMaker including versions of
-        //         Lorem Ipsum.
-        //     </div>
-        //     <div>map</div>
-        // </div>
-        <div>
-            <Button
-                onClick={() => {
-                    setScroll('paper');
-                    setOpen(true);
-                }}
-            >
-                scroll
-            </Button>
-
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                scroll={scroll}
-                aria-labelledby='scroll-dialog-title'
-                aria-describedby='scroll-dialog-description'
-            >
-                <DialogContent>
-                    <div className='flex flex-row justify-around child:w-32 child:h-32 bg-primary-100'>
-                        <div>1</div>
-                        <div>1</div>
-                        <div>1</div>
+        <div className='flex flex-col justify-around bg-gray-100'>
+            <div className='flex flex-row justify-evenly'>
+                <div className='flex flex-col justify-start my-3'>
+                    <div className='text-2xl'>Cool match</div>
+                    <div className='text-xs'>Organized by Eric Erricson</div>
+                </div>
+                <div className='flex flex-col justify-start my-3'>
+                    <Button
+                        className='mb-5 w-52 bg-gray-300'
+                        sx={{ color: 'black' }}
+                    >
+                        Request ride
+                    </Button>
+                    <div className='my-1 w-fit h-fit text-xs text-gray-400'>
+                        Thur, April 26, 2066 at 22:30 - Thur, April 26, 2066 at
+                        23:30
                     </div>
-                </DialogContent>
+                    <div className='my-1 w-fit h-fit text-xs text-gray-400'>
+                        kungsträdgården
+                    </div>
+                </div>
+                <div className='my-5 w-14 h-14 bg-primary-100'>
+                    <div className=' text-xl'>APR 23</div>
+                </div>
+            </div>
 
-                <DialogActions>
-                    <Button onClick={handleClose}>Save and exit</Button>
-                </DialogActions>
-            </Dialog>
+            <div className='flex flex-row justify-around py-3 my-3 bg-gray-300'>
+                <div>Description</div>
+                <div>Carpooling</div>
+                <div>Events</div>
+            </div>
+            <div className='flex flex-row justify-around py-3 my-3 bg-gray-300'>
+                <CarPooler>CarPooler 1</CarPooler>
+                <CarPooler>CarPooler 2</CarPooler>
+                <CarPooler>CarPooler 3</CarPooler>
+            </div>
+            <Divider variant='middle' />
+            <div className='my-1 w-fit h-fit text-xs text-gray-700'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industries standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+            </div>
+            <div>map</div>
         </div>
+//The above and below code must be merged
+        // <div>
+        //     <Button
+        //         onClick={() => {
+        //             setScroll('paper');
+        //             setOpen(true);
+        //         }}
+        //     >
+        //         scroll
+        //     </Button>
+
+        //     <Dialog
+        //         disablePortal
+        //         open={open}
+        //         onClose={handleClose}
+        //         scroll={scroll}
+        //         aria-labelledby='scroll-dialog-title'
+        //         aria-describedby='scroll-dialog-description'
+        //     >
+        //         <DialogContent>
+        //             <div className='flex flex-row justify-around child:w-32 child:h-32 bg-primary-100'>
+        //                 <div>1</div>
+        //                 <div>1</div>
+        //                 <div>1</div>
+        //             </div>
+        //         </DialogContent>
+
+        //         <DialogActions>
+        //             <Button onClick={handleClose}>Save and exit</Button>
+        //         </DialogActions>
+        //     </Dialog>
+        // </div>
     );
 }
