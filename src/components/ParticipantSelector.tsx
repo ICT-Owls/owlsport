1b2a615203ed9f2633d364e0df54a76136ba82a3
Hugo Sandell <hugossandell@gmail.com>
2022-05-05 10:38:20 +0200

    chore: add participant selector component

---
+++ src/components/ParticipantSelector.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Dialog, DialogTitle, Button } from '@mui/material';
+
+type ParticipantSelectorProps = {
+    a: string;
+};
+
+export default function ParticipantSelector(props: {}) {
+    //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
+    return (
+        <Button>t</Button>
+    );
+}
