ad7883a63e2ae804e71f1962484192f6c3e60833
VolcanoCookies <franek.kg@gmail.com>
2022-05-10 10:36:06 +0200

    style: more object destructuring

--- src/presenters/EventListPresenter.js
+++ src/presenters/EventListPresenter.js
@@ -1,30 +1,30 @@
 import React, { useEffect } from 'react';
 import { eventApi } from '../helpers/Firebase';
 import EventListView from '../views/EventListView';
-export default function EventListPresenter(props) {
+export default function EventListPresenter({ user }) {
     const [events, setEvents] = React.useState([]);
 
     useEffect(() => {
-        if (props.user) {
+        if (user) {
             eventApi
                 .eventsGet({
                     headers: {
-                        authorization: `Bearer ${props.user.accessToken}`,
+                        authorization: `Bearer ${user.accessToken}`,
                     },
                 })
                 .then((data) =>
                     setEvents(
                         data.map((e) => {
-                            if (!e.members) e.members = ['nomembers'];
+                            if (!e.members) e.members = [];
                             return e;
                         })
                     )
                 )
                 .catch((err) => console.error(err));
         }
-    }, [props.user]);
+    }, [user]);
 
     // Hooks, logic, etc goes here. These presenters manipulate data, transform it into usable functions and values, then passes those to a view.
     // No visual code here.
-    return EventListView({ events: events, user: props.user });
+    return EventListView({ events, user });
 }
