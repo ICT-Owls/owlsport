6d86e4a56ffee1fbd6894faf2cedeb9bd89440ae
VolcanoCookies <franek.kg@gmail.com>
2022-05-12 15:21:32 +0200

    style: formatting

--- src/components/MainContentContainer.tsx
+++ src/components/MainContentContainer.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 
 const MainContentContainer = (props: any) => {
     return (
-        <Box sx={{ marginX: '2rem', minHeight: '50vh', maxWidth: '980px', width:'100%'}}>
-            <div className={'h-full w-full bg-background-100 p-0 shadow-md bg-background-100 bg-opacity-80'}>
+        <Box
+            sx={{
+                marginX: '2rem',
+                minHeight: '50vh',
+                maxWidth: '980px',
+                width: '100%',
+            }}
+        >
+            <div className="h-full w-full rounded-tr-md rounded-br-md  bg-background-100 bg-background-100 bg-opacity-80 p-0 shadow-md">
                 {props.children}
             </div>
         </Box>
