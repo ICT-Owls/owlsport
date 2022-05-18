8b7d7e5bd233440086bfaaceca1ec02e09be27eb
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-05-18 14:19:12 +0200

    Feat: finalized style for navbar buttons

--- src/views/NavbarView.jsx
+++ src/views/NavbarView.jsx
@@ -8,11 +8,13 @@ import PeopleIcon from '@mui/icons-material/People';
 import ChatIcon from '@mui/icons-material/Chat';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useState } from 'react';
-import { useChat } from 'models/Model';
+import { useChat, useUser } from 'models/Model';
+import AvatarView from './AvatarView';
 
 export default function NavbarView() {
     const [anchorEl, setAnchorEl] = useState(null);
     const [chat, setChat] = useChat();
+    const [user] = useUser;
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -24,15 +26,22 @@ export default function NavbarView() {
     return (
         <div
             className={
-                'fixed z-50 grid h-fit w-full grid-cols-6 bg-background-100 shadow-sm'
+                'fixed z-50 grid h-fit w-full grid-cols-12 bg-background-100 shadow-sm lg:grid-cols-6'
             }
         >
-            <Link to="/" className={'flex items-center justify-center'}>
+            <Link
+                to="/"
+                className={'col-span-1 flex items-center justify-center'}
+            >
                 <img src="Logotype.png" className={'h-10'} alt="" />
             </Link>
-            <div className={'col-span-4 m-2 flex w-full justify-center'}>
+            <div
+                className={
+                    'col-span-10 m-2 flex w-full justify-center lg:col-span-4'
+                }
+            >
                 <TextField
-                    className="max-w-4xl"
+                    className="w-2/3 max-w-3xl"
                     placeholder="search"
                     fullWidth
                     id="navbar-search"
@@ -48,23 +57,27 @@ export default function NavbarView() {
                 </IconButton>
             </div>
             <div className=" col-span-1 flex justify-end pr-5">
-                <IconButton className="hidden xl:block">
+                <IconButton className="hidden lg:block">
                     <NotificationsNoneIcon />
                 </IconButton>
                 <IconButton
-                    className="hidden xl:block"
+                    className="hidden lg:block"
                     onClick={() => setChat(!chat)}
                 >
                     <ChatIcon />
                 </IconButton>
-                <IconButton className="hidden xl:block">
+                <IconButton className="hidden lg:block">
                     <PeopleIcon />
                 </IconButton>
-                <IconButton className="hidden xl:block">
+                <IconButton className="hidden lg:block">
                     <SettingsIcon />
                 </IconButton>
+                <IconButton className="hidden lg:block">
+                    <AvatarView maxHeight="100%" user={user} />
+                </IconButton>
+
                 <IconButton
-                    className="xl:hidden"
+                    className="lg:hidden"
                     id="basic-button"
                     onClick={handleClick}
                 >
@@ -76,6 +89,13 @@ export default function NavbarView() {
                     open={open}
                     onClose={handleClose}
                 >
+                    <MenuItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                    >
+                        <AvatarView maxHeight="100%" user={user} />
+                    </MenuItem>
                     <MenuItem
                         onClick={() => {
                             handleClose();
