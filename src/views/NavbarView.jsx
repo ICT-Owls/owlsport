f6112658cc84da82c46c74358b71ddc0b9d72390
samsonsin <23201902+samsonsin@users.noreply.github.com>
2022-05-18 12:36:19 +0200

    Feat: Updated navbar styling & buttons Added a menu for small screens & a set of icons for future buttons. Messages icon toggles useChat hook

--- src/views/NavbarView.jsx
+++ src/views/NavbarView.jsx
@@ -1,58 +1,39 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { IconButton, TextField } from '@mui/material';
+import { IconButton, TextField, Menu, MenuItem } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PeopleIcon from '@mui/icons-material/People';
+import ChatIcon from '@mui/icons-material/Chat';
+import { useState } from 'react';
+import { useChat } from 'models/Model';
 
 export default function NavbarView() {
-    //These views only handle UI. They should not handle any logic outside of ui (They can handle logic specific to some ui element, if neccessary)
-    /*
-    return (
-        <nav className="navbar is-fresh"
-        role='navigaion'
-        aria-label='main navigation'>
-            <IconButton color="secondary" aria-label="Search" component="span">
-                <img src="Logotype.png" className="h-10" alt="" />
-            </IconButton>
-            <Box
-                className="flex justify-start m-4"
-                sx={{
-                    width: 1000,
-                    maxWidth: '100%',
-                }}
-            >
-                <TextField
-                    fullWidth
-                    label="fullWidth"
-                    id="fullWidth"
-                    color="secondary"
-                />
-                <IconButton
-                    color="secondary"
-                    aria-label="Search"
-                    component="span"
-                >
-                    <SearchIcon color="secondary" sx={{ fontSize: 40 }} />
-                </IconButton>
-            </Box>
-        </nav>
-    );
-    /*/
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [chat, setChat] = useChat();
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div
             className={
-                'bg-primary fixed z-50 flex h-fit w-full justify-around bg-background-100 shadow-sm'
+                'fixed z-50 grid h-fit w-full grid-cols-6 bg-background-100 shadow-sm'
             }
         >
-            <Link
-                to="/"
-                className={'ml-8 mr-8 flex items-center justify-center'}
-            >
+            <Link to="/" className={'flex items-center justify-center'}>
                 <img src="Logotype.png" className={'h-10'} alt="" />
             </Link>
-            <div className={'m-2 flex w-full justify-start'}>
+            <div className={'col-span-4 m-2 flex w-full justify-center'}>
                 <TextField
-                    fullWidth
+                    className="max-w-5xl"
                     placeholder="search"
+                    fullWidth
                     id="navbar-search"
                     color="secondary"
                     size="small"
@@ -64,8 +45,57 @@ export default function NavbarView() {
                 >
                     <SearchIcon color="secondary" />
                 </IconButton>
-            </div>{' '}
-            <div>{/* I act as a third item for formattings sake */}</div>
+            </div>
+            <div className=" col-span-1 flex justify-end">
+                <IconButton className="hidden md:block">
+                    <NotificationsNoneIcon />
+                </IconButton>
+                <IconButton
+                    className="hidden md:block"
+                    onClick={() => setChat(!chat)}
+                >
+                    <ChatIcon />
+                </IconButton>
+                <IconButton className="hidden md:block">
+                    <PeopleIcon />
+                </IconButton>
+                <IconButton
+                    className="md:hidden"
+                    id="basic-button"
+                    onClick={handleClick}
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <MenuItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                    >
+                        <NotificationsNoneIcon />
+                    </MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            handleClose();
+                            setChat(!chat);
+                        }}
+                    >
+                        <ChatIcon />
+                    </MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                    >
+                        <PeopleIcon />
+                    </MenuItem>
+                </Menu>
+            </div>
         </div>
     );
     //*/
