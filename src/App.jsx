6699cc2722263d9d1fed02fe68b9eb44feb1b1f3
Hugo Sandell <hugossandell@gmail.com>
2022-05-20 21:39:37 +0200

    style: organise imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-/*  Presenters  */
-import { Box } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { GoogleMapsLoader } from 'helpers/Location';
-import {Status as MapStatus} from '@googlemaps/react-wrapper';
 import ChatsPresenter from 'presenters/ChatsPresenter';
 import React, { useEffect, useLayoutEffect } from 'react';
 import './App.css';
-import { initModel, useUser, useChat, useMapStatus } from './models/Model';
+import { initModel, useUser, useChat } from './models/Model';
 import MainContentPresenter from './presenters/MainContentPresenter';
 import NavbarPresenter from './presenters/NavbarPresenter';
 import SidebarPresenter from './presenters/SidebarPresenter';
@@ -29,27 +26,27 @@ export default function App() {
 
     return (
         <StyledEngineProvider injectFirst>
-        <GoogleMapsLoader>
-            <ThemeProvider theme={lightmode ? LightTheme : DarkTheme}>
+            <GoogleMapsLoader>
+                <ThemeProvider theme={lightmode ? LightTheme : DarkTheme}>
                     <NavbarPresenter />
 
-                <div
-                    className="App fixed flex h-screen w-screen flex-row justify-center child:mt-20"
-                    style={{
-                        backgroundImage: `url('../stacked-waves-haikei.svg')`,
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'left',
-                        backgroundSize: 'cover',
-                        backgroundAttachment: 'fixed',
-                    }}
-                >
-                    <SidebarPresenter user={user} />
-                    <div className="h-full w-10 bg-gradient-to-r from-sideandmain-100 to-sideandmain-200" />
-                    <MainContentPresenter user={user} />
-                    {chat && <ChatsPresenter />}
-                </div>
-            </ThemeProvider>
-        </GoogleMapsLoader>
+                    <div
+                        className="App fixed flex h-screen w-screen flex-row justify-center child:mt-20"
+                        style={{
+                            backgroundImage: `url('../stacked-waves-haikei.svg')`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'left',
+                            backgroundSize: 'cover',
+                            backgroundAttachment: 'fixed',
+                        }}
+                    >
+                        <SidebarPresenter user={user} />
+                        <div className="h-full w-10 bg-gradient-to-r from-sideandmain-100 to-sideandmain-200" />
+                        <MainContentPresenter user={user} />
+                        {chat && <ChatsPresenter />}
+                    </div>
+                </ThemeProvider>
+            </GoogleMapsLoader>
         </StyledEngineProvider>
     );
 }
