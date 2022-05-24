47af78f97edc4579d7bd0d11242cb8dee7caeb1b
Hugo Sandell <hugossandell@gmail.com>
2022-05-24 09:31:09 +0200

    fix: remove unnecessary div from welcomeview

--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import MainContentContainer from 'components/MainContentContainer';
 
 const WelcomeView = () => {
-    return (<div className='px-20 py-5 h-full bg-background-100'>
-        <h1 className="font-">Welcome to OwlSport!</h1>
-        Please sign in to access your event list.
-    </div>);
+    return (
+        <>
+            <h1 className="font-">Welcome to OwlSport!</h1>
+            Please sign in to access your event list.
+        </>
+    );
 };
 
 export default WelcomeView;
